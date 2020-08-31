cf6fc89e40332c7512104a315bda1aef3b1add14
Mark <mark.litfin@yahoo.com>
2020-08-31 18:53:58 -0400

    added about and contact components

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const About = () => {
+    return (
+    <main>
+        <section className="content_section">
+            <section className="column">
+
+            <div className="row">
+                <div className="col-lg-6">
+                
+                    <div className="column-content bio">
+                    <h1>About</h1>
+                    <p>
+                        
+                    </p>
+                    </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="column-content">
+                        <img src={process.env.PUBLIC_URL + "/images/Mark_Litfin_pic.jpg"} className="img-fluid" alt="Mark was Here" />
+                    </div>
+                    </div>
+            </div>
+            </section>
+        </section>
+    </main>
+    );
+}
+
+export default About;
