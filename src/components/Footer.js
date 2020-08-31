341a59df302ce05887c2a8f75cfd56a2f05cfc06
Mark <mark.litfin@yahoo.com>
2020-08-31 18:56:29 -0400

    added the footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+    <footer>
+        <p>Designed and Developed by Mark Litfin | Miami, FL</p>
+    </footer>
+    );
+}
+
+export default Footer;
