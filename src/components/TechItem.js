1d3e52c21f531f6e9c6368d1df06856ce547e147
Mark <mark.litfin@yahoo.com>
2020-09-05 21:24:25 -0400

    updated the project.json

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 function TechItem(props) {
 
-    const image = props.techImg;
     const name = props.techName;
 
     return (
     <div className="tech-icons">
-        <img src={process.env.PUBLIC_URL + "images/techicons" + image} width="70" alt={name} />
-        <br />
         {name}
     </div>
 )
--- src/components/project-components/Project.js
+++ src/components/project-components/Project.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import TechItem from '../TechItem';
+import TechItem from '../Techitem.js';
 
 const Project = props => {
 
